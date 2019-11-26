87f1452c5737e559176bd2e248b08899fd4bf93c
awaseem2 <awaseem2@illinois.edu>
2019-11-25 23:23:48 -0600

    works as it should

--- crawler.py
+++ crawler.py
@@ -14,9 +14,8 @@ util.prompt_for_user_token('aly5321', 'user-follow-read',
     redirect_uri = 'https://www.spotify.com/us/')
 '''
 
-arists = []
-albums = []
 artists_to_add = []
+albums_to_add = []
 
 def init_artist_ids():
     artist_ids['5 Seconds of Summer'] = 1
@@ -27,6 +26,8 @@ def init_artist_ids():
     artist_ids['Harry Styles'] = 6
     artist_ids['Niall Horan'] = 7
     artist_ids['Zayn Malik'] = 8
+    artist_ids['Demi Lovato'] = 9
+    artist_ids['Grey'] = 10
 
 def init_album_ids():
     album_ids['5 Seconds of Summer'] = 1
@@ -35,21 +36,34 @@ def init_album_ids():
     album_ids['Cuz I Love You'] = 4
     album_ids['Folie a Deux'] = 5
     album_ids['DAMN'] = 6
+    album_ids['Confident'] = 7
+    album_ids['Grey'] = 8
 
-def get_artist_id(artist):
+#artist = name, id_ = spotify's artist id, not our artist id
+def get_artist_id(artist, id_):
     if artist in artist_ids:
         return artist_ids[artist]
     new_id = len(artist_ids) + 1
-    artist_ids[artist] = new_id
 
-    #insert = "INSERT INTO `Artist`(`Artist_ID`, `Artist_Name`, `Artist_Popularity`, `Artist_Genre`) VALUES ({},'{}',{},'{}')".format(new_id)
-    #artists_to_add.append("artist_ids['" + artist_name + "'] = " + str(new_id))
+    popularity = sp.artist(id_)['popularity']
+    genre = sp.artist(id_)['genres'][0] if len(sp.artist(id_)['genres']) > 0 else "Pop"
+    insert = "INSERT INTO `Artist`(`Artist_ID`, `Artist_Name`, `Artist_Popularity`, `Artist_Genre`) VALUES ({},'{}',{},'{}')".format(new_id, artist, popularity, genre)
+    
+    artists.append(insert)
+    artists_to_add.append("artist_ids['" + artist + "'] = " + str(new_id))
+    artist_ids[artist] = new_id
     return new_id
 
-def get_album_id(album):
+def get_album_id(album, id_, artist, art_id_):
     if album in album_ids:
         return album_ids[album]
     new_id = len(album_ids) + 1
+
+    genre = sp.album(id_)['genres'][0] if (len(sp.album(id_)['genres']) > 0) else "Pop"
+    artist_id = get_artist_id(artist, art_id_)
+    insert = "INSERT INTO `Album`(`Album_ID`, `Album_Name`, `Album_Genre`, `Artist_ID`) VALUES ({},'{}','{}',{})".format(new_id, album, genre, artist_id)
+    albums.append(insert)
+    albums_to_add.append("album_ids['" + album + "'] = " + str(new_id))
     album_ids[album] = new_id
     return new_id
 
@@ -69,7 +83,9 @@ init_artist_ids()
 init_album_ids()
 
 songs = []
-user_info = []
+users = []
+artists = []
+albums = []
 
 if token:
     sp = spotipy.Spotify(auth=token)
@@ -84,7 +100,6 @@ if token:
         time = item['added_at']
         date = time[0 : time.find('T')]
         hour = time[time.find('T') + 1 : time.find('Z') - 3].replace(":", "")
-        print(hour)
         name = track['name']
         artist = track['artists'][0]['name']
         album = track['album']['name']
@@ -98,15 +113,48 @@ if token:
         genres = sp.artist(track['artists'][0]['id'])['genres']
         genre = genres[0] if (len(genres) > 0) else 'Pop'
 
-        artist_id = get_artist_id(artist)
-        album_id = get_album_id(album)
+        artist_id = get_artist_id(artist, track['artists'][0]['id'])
+        album_id = get_album_id(album, track['album']['id'], artist, track['artists'][0]['id'])
 
         insert = "INSERT INTO `Song`(`Song_ID`, `Song_Name`, `Song_Tempo`, `Song_Danceability`, `Song_Acoustics`, `Song_Energy`, `Song_Valence`, `Artist_ID`, `Album_ID`, `Song_Genre`) VALUES ('{}', '{}',{},{},{},{},{},{},{},'{}')".format(track_id, name, tempo, danceability, acousticness, energy, valence, artist_id, album_id, genre)
         songs.append(insert)
-        #print(insert)
-        insert2 = "INSERT INTO `User`(`User_ID`, `Song_ID`, `Time`, `Date`) VALUES ('{}','{}',{},{})".format(sys.argv[1], track_id, hour, date)
-        user_info.append(insert2)
-        break
+        insert2 = "INSERT INTO `User`(`User_ID`, `Song_ID`, `Time`, `Date`) VALUES ('{}','{}',{},'{}')".format(sys.argv[1], track_id, hour, date)
+        users.append(insert2)
 else:
     print("Can't get token for", username)
 
+f = open("songs.txt", "w")
+for song in songs:
+    f.write(song + ";")
+    f.write("\n")
+f.close()
+
+f = open("user.txt", "w")
+for user in users:
+    f.write(user + ";")
+    f.write("\n")
+f.close()
+
+f = open("albums.txt", "w")
+for album in albums:
+    f.write(album + ";")
+    f.write("\n")
+f.close()
+
+f = open("artists.txt", "w")
+for artist in artists:
+    f.write(artist + ";")
+    f.write("\n")
+f.close()
+
+f = open("artist_to_add.txt", "w")
+for artist in artists_to_add:
+    f.write(artist)
+    f.write("\n")
+f.close()
+
+f = open("albums_to_add.txt", "w")
+for album in albums_to_add:
+    f.write(album)
+    f.write("\n")
+f.close()
