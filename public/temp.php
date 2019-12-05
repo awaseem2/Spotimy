0c52fce0c161e8098f3f848f0683979b4af71935
irdina <32141959+dinasaur3@users.noreply.github.com>
2019-12-05 02:33:15 -0600

    Update temp.php

--- public/temp.php
+++ public/temp.php
@@ -5,7 +5,5 @@ if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
-$user_update = $mysqli->query("UPDATE `User` SET `Song_ID`= '".$_POST["userid"]."'
-WHERE User_ID = 'admin'");
 header('Location: https://spotimyapp.web.illinois.edu/user_index.html');
 $mysqli->close();
