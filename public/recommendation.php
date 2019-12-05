0fbf31137205f201f328d50103ba989f220ea863
irdina <32141959+dinasaur3@users.noreply.github.com>
2019-12-05 02:14:06 -0600

    Update recommendation.php

--- public/recommendation.php
+++ public/recommendation.php
@@ -6,22 +6,7 @@ if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 
-$correct_username = $mysqli->query("SELECT COUNT(*) FROM User WHERE '".$_POST["user"]."' = (SELECT Song_ID FROM User WHERE User_ID = 'admin')");
-$row0 = mysqli_fetch_row($correct_username);
-$can_change = $row0[0];
-
-if($can_change == 0){
- echo'
-   <script>
-   window.onload = function() {
-      alert("Please enter your username.");
-      location.href = "user_index.html";  
-   }
-   </script>
-';
-} else {
-
-$proc = 
+$rec = 
 "DROP TABLE IF EXISTS last_playlist;
 CREATE PROCEDURE recom AS
 SELECT DISTINCT S1.Song_Name, A.Artist_Name INTO last_playlist
@@ -62,33 +47,6 @@ IF (0 = SELECT COUNT(*) FROM new_playlist
 BEGIN
     EXEC recom
 END;";
-
-$rec = "SELECT DISTINCT S1.Song_Name, A.Artist_Name, S1.Song_ID as Song_ID 
-FROM Song S1 
-INNER JOIN User U ON U.SONG_ID <> S1.Song_ID AND U.User_ID = '".$_POST["user"]."'
-INNER JOIN Artist A ON S1.Artist_ID = A.Artist_ID 
-WHERE
-S1.Song_Genre = (SELECT genre_name
-FROM (SELECT s.Song_Genre genre_name, COUNT(s.Song_Genre) genre_count 
-FROM Song s 
-UNION 
-SELECT a.Artist_Genre genre_name, COUNT(a.Artist_Genre) genre_count 
-FROM Artist a 
-UNION 
-SELECT b.Album_Genre genre_name, COUNT(b.Album_Genre) genre_count 
-FROM Album b
-GROUP BY genre_name 
-ORDER BY genre_count ) as song_genre 
-GROUP BY genre_name 
-HAVING MAX(genre_count) 
-ORDER BY MAX(genre_count) DESC
-LIMIT 1)
-AND Song_Tempo BETWEEN (SELECT AVG(Song_Tempo) - 25 FROM Song) AND (SELECT AVG(Song_Tempo) + 25 FROM Song)
-OR Song_Danceability BETWEEN (SELECT AVG(Song_Danceability) -.2 FROM Song) AND (SELECT AVG(Song_Danceability) +.2 FROM Song)
-OR Song_Acoustics BETWEEN (SELECT AVG(Song_Acoustics) -.2 FROM Song) AND (SELECT AVG(Song_Acoustics) +.2 FROM Song)
-OR Song_Energy BETWEEN (SELECT AVG(Song_Energy) -.2 FROM Song) AND (SELECT AVG(Song_Energy) +.2 FROM Song)
-ORDER BY rand()
-LIMIT 20";
 
 $search_result = $mysqli->query($rec);
 
